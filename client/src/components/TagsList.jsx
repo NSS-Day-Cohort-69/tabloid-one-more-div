29b266fe16fb04b53ddfd980e399e3d19f73b9da
Chad Cote <chadcote58@gmail.com>
2024-05-29 16:07:22 -0400

    Rendered all of the tags with edit and delete buttons

--- client/src/components/TagsList.jsx
+++ client/src/components/TagsList.jsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react"
+import { getAllTags } from "../managers/tagManager.js"
+import { Button, ButtonToolbar, Card, CardBody, CardTitle } from "reactstrap"
+
 export default function AllTags()
 {
+    const [tag, setTag]= useState([])
+
+    useEffect(() => {
+        getAllTags().then(setTag)
+    },[])
     return(
-        <div>hello</div>
+        <>
+        <h4 style={{display: 'flex', justifyContent: 'center'}}>Available Tags</h4>
+        {tag.map((t) => (
+        <Card className="mt-4 w-25 m-auto">
+            <CardBody className="d-flex align-items-center justify-content-between">
+                <CardTitle className="fw-bold">
+                {t.name}
+                </CardTitle>
+                <ButtonToolbar className="gap-2 "style={{float: "right"}} >
+                <Button color="primary" >Edit</Button>
+                <Button color="danger">DELETE</Button>
+                </ButtonToolbar>
+            </CardBody>
+        </Card>
+
+        ))}
+
+        </>
     )
 }
