a13260f11efb2ce4b2a8c9008386a68edf15de55
Chad Cote <chadcote58@gmail.com>
2024-05-31 11:58:47 -0400

    Rendered the users details in a card

--- client/src/components/userprofiles/UserProfileDetails.jsx
+++ client/src/components/userprofiles/UserProfileDetails.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getProfile } from "../../managers/userProfileManager";
+import { Card, CardBody, CardImg, CardText, Label } from "reactstrap";
 
 export default function UserProfileDetails() {
   const [userProfile, setUserProfile] = useState();
@@ -15,10 +16,34 @@ export default function UserProfileDetails() {
     return null;
   }
   return (
-    <>
-      <img src={userProfile.imageLocation} alt={userProfile.firstName} />
-      <h3>{userProfile.fullName}</h3>
-      <p>Username: {userProfile.userName}</p>
-    </>
+    <Card className="w-50 m-auto">
+       <div className="">
+      <img alt="user profile image" className="w-50"
+      src={userProfile.imageLocation} style={{height: 200, borderRadius: "50%"}}/>
+
+      </div>
+      <CardBody>
+        <Label className="fw-bold">FullName:</Label>
+        <CardText>
+        {userProfile.fullName}
+        </CardText>
+        <Label className="fw-bold">UserName:</Label>
+        <CardText>
+          {userProfile.userName}
+        </CardText>
+        <Label className="fw-bold">Email:</Label>
+        <CardText>
+          {userProfile.email}
+        </CardText>
+        <Label className="fw-bold">Creation Date:</Label>
+        <CardText>
+          {userProfile.createDateTime}
+        </CardText>
+        <Label className="fw-bold">FullName:</Label>
+        <CardText>
+          {userProfile.roles}
+        </CardText>
+      </CardBody>
+    </Card>
   );
 }
