69d7d0639df22ba45cfccbf7d40143b3f2cfb931
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-05-31 21:20:16 -0500

    Made Post Titles and Authors clickable, navigating to related Post Details or User Profile view

--- client/src/components/posts/PostList.jsx
+++ client/src/components/posts/PostList.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { getAllApprovedAndPublishedPosts } from "../../managers/postManager"
 import PageContainer from "../PageContainer"
-import { Badge, Card, CardSubtitle, CardText } from "reactstrap"
+import { Badge, Card, CardLink } from "reactstrap"
 
 export const PostList = ({ loggedInUser }) => {
     const [posts, setPosts] = useState([])
@@ -19,10 +19,16 @@ export const PostList = ({ loggedInUser }) => {
                 return (
                     <Card className="w-75 shadow-sm p-3 pb-2" style={{maxWidth: "1200px"}} outline color="light" key={`post-${p.id}`}>
                         <div className="d-flex align-items-center gap-2">
-                            <Badge className="fs-6 mb-1" pill>{p.category.name}</Badge>
-                            <CardSubtitle><em>{p.userProfile.fullName}</em></CardSubtitle>
+                            <Badge className="fs-6" pill>{p.category.name}</Badge>
+                            <CardLink className="text-black text-decoration-none" href={`userprofiles/${p.userProfile.id}`}>
+                                <em>{p.userProfile.fullName}</em>
+                            </CardLink>
                         </div>
-                        <CardText className="fs-2 fw-bold">{p.title}</CardText>
+                        <div>
+                            <CardLink className="fs-2 fw-bold text-black text-decoration-none" href={`posts/${p.id}`}>
+                                {p.title}
+                            </CardLink>
+                        </div>
                     </Card>
                 )
             })}
