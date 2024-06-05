82e186becc4312602c9728c85bc26e7548671d1a
Chad Cote <chadcote58@gmail.com>
2024-06-05 11:03:19 -0400

    Rendered each unapproved post including a approve button

--- client/src/components/posts/ApprovePost.jsx
+++ client/src/components/posts/ApprovePost.jsx
@@ -1,107 +1,64 @@
 import { useEffect, useState } from "react"
+import { getUnapprovedPosts } from "../../managers/postManager.js"
+import PageContainer from "../PageContainer.jsx"
+import { Badge, Button, Card, CardBody, CardImg, CardImgOverlay, CardSubtitle, CardText, CardTitle } from "reactstrap"
 
-export default function ApprovePost(){
+export default function ApprovePost({loggedInUser}){
 
     const [unapprovedPosts, setUnapprovedPosts] = useState([])
+
+
+    useEffect(() => {
+        getUnapprovedPosts().then(setUnapprovedPosts)
+    },[])
     return (
         <PageContainer>
-            <Card className="w-75 shadow" style={{maxWidth: "1200px"}} outline color="light" key={post.id}>
-                {post.headerImageURL && (
-                    <>
-                        <CardImg 
-                            alt="Post Header Image"
-                            src={post.headerImageURL}
-                            width="100%"
-                            className="mb-1"
-                        />
-                        <CardImgOverlay className="pe-none">
-                            <Badge className="fs-3 fw-bold mb-2 shadow pe-auto">
-                                {post.title}
-                            </Badge>
-                            <div className="pe-auto">
-                                <Badge className="fs-6 mb-2 shadow" pill>{post.category?.name}</Badge>
-                            </div>
-                        </CardImgOverlay>
-                    </>
-                )}
-                <CardBody className="pt-1">
-                    {!post.headerImageURL && (
+            <h2>Unapproved Posts</h2>
+            {unapprovedPosts.map(up => {
+                return(
+                    <Card className="w-75 shadow" style={{maxWidth: "1200px"}} outline color="light" key={up.id}>
+                    {up.headerImageURL && (
                         <>
-                            <CardTitle className="fs-2 fw-bold mb-0">{post.title}</CardTitle>
-                            <div>
-                                <Badge className="fs-6" pill>{post.category?.name}</Badge>
-                            </div>
+                            <CardImg 
+                                alt="Up Header Image"
+                                src={up.headerImageURL}
+                                width="100%"
+                                className="mb-1"
+                            />
+                            <CardImgOverlay className="pe-none">
+                                <Badge className="fs-3 fw-bold mb-2 shadow pe-auto">
+                                    {up.title}
+                                </Badge>
+                                <div className="pe-auto">
+                                    <Badge className="fs-6 mb-2 shadow" pill>{up.category?.name}</Badge>
+                                </div>
+                            </CardImgOverlay>
                         </>
                     )}
-                    {post.tags.length > 0 && (
-                        <div className="d-flex gap-2 mb-3 pt-2">
-                            {post.tags.map(t => {
-                                return (
-                                    <Badge color="info" key={`tag-${t.id}`} pill>{t.name}</Badge>
-                                )
-                            })}
-                        </div>
-                    )}
-                    <CardSubtitle className="fw-bold">{post.userProfile.fullName}</CardSubtitle>
-                    <CardSubtitle>{post.formattedPublicationDate}</CardSubtitle>
-                    <CardText className="mt-3">{post.content}</CardText>
-                    <div className="d-flex flex-column align-items-end">
-                        <div className="d-flex flex-row flex-wrap gap-1">
-                            {post.reactions.map(r => {
-                                if (r.postReactions.some(pr => pr.userProfileId == loggedInUser.id)) {
-                                    return (
-                                        <Button 
-                                            className="px-1 pe-2 py-0" 
-                                            color="primary" 
-                                            title={r.name} 
-                                            onClick={() => handleDeletePostReaction(r.id)}
-                                            key={`reaction-${r.id}`}
-                                        >
-                                            {`${r.reactionImage} ${r.postReactionsCount}`}
-                                        </Button>
-                                    )
-                                } else {
-                                    return (
-                                        <Button 
-                                            className="px-1 pe-2 py-0" 
-                                            title={r.name} 
-                                            onClick={() => handleCreatePostReaction(r.id)}
-                                            key={`reaction-${r.id}`}
-                                        >
-                                            {`${r.reactionImage} ${r.postReactionsCount}`}
-                                        </Button>
-                                    )
-                                }
-                            })}
-                        </div>
-                        <div className="d-flex flex-row flex-wrap mt-3 w-100 gap-2">
-                            <div className="d-flex flex-fill">
-                                {loggedInUser.id != post.userProfileId && (
-                                    <Button>Subscribe</Button>
+                    <CardBody className="pt-1">
+                        {!up.headerImageURL && (
+                            <>
+                                <CardTitle className="fs-2 fw-bold mb-0">{up.title}</CardTitle>
+                                <div>
+                                    <Badge className="fs-6" pill>{up.category?.name}</Badge>
+                                </div>
+                            </>
+                        )}
+                        <CardSubtitle className="fw-bold">{up.userProfile.fullName}</CardSubtitle>
+                        <CardSubtitle>{up.formattedPublicationDate}</CardSubtitle>
+                        <CardText className="mt-3">{up.content}</CardText>
+                            <div className="d-flex flex-row flex-wrap mt-3 w-100 gap-2">
+                                {(loggedInUser.id == up.userProfileId || loggedInUser.roles.includes("Admin")) && (
+                                    <Button>Delete</Button>
                                 )}
+                            <Button >Approve</Button>
                             </div>
-                            <Button>{`${post.commentsCount} Comments`}</Button>
-                            <Button>Create A Comment</Button>
-                            {loggedInUser.id == post.userProfileId && (
-                                <>
-                                    <Button onClick={toggleModal}>Manage Tags</Button>
-                                    <Button>Edit</Button>
-                                </>
-                            )}
-                            {(loggedInUser.id == post.userProfileId || loggedInUser.roles.includes("Admin")) && (
-                                <Button>Delete</Button>
-                            )}
-                        </div>
-                    </div>
-                </CardBody>
-            </Card>
-            <PostTagsModal
-                refresh = {refresh}
-                isModalOpen={isModalOpen}
-                toggleModal={toggleModal}
-                allTags={allTags}
-                post={post}
-            />
+                    </CardBody>
+                </Card>
+                )
+  
+            })}
+            
         </PageContainer>
     )
 }
